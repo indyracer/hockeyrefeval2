b5cfd4bcc31284aa173738f3c638c9c513dc374e
Bob Cohen <bobcohen1969@yahoo.com>
2016-11-28 11:30:44 -0600

    first commit

---
+++ src/main/java/org/launchcode/RefEvalApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefEvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefEvalApplication.class, args);
+	}
+}
